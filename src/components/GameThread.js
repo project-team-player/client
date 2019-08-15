2a032fd9fc39ec6e88f51e69422533b0a8cca2e6
Hans Martin <hansmhank@gmail.com>
2019-08-15 16:16:44 -0700

    basic structure done

--- src/components/GameThread.js
+++ src/components/GameThread.js
@@ -33,14 +33,16 @@ const GameThread = () => {
       <div className="game-thread-content">
       <div className="teams">
         <img src={BearsLogo} />
-        <span className="team-name">Chicago Bears</span>
-        <span className="vs">VS</span>
-        <span className="team-name">New England Patriots</span>
+        <div className="team-text">
+          <span className="team-name">Chicago Bears</span>
+          <span className="vs">VS</span>
+          <span className="team-name">New England Patriots</span>
+        </div>
         <img src={PatriotsLogo} />
       </div>
 
       <div className="predictions">
-        <h2>User Predictions</h2>
+        <h2>Who's Got Sauce?</h2>
         <div className="prediction-counter">
           <div className="prediction home-team-prediction"><span>25%</span></div>
           <div className="prediction away-team-prediction"><span>75%</span></div>
@@ -50,77 +52,112 @@ const GameThread = () => {
       <div className="discussion-container">
         <h2>Place Your Slices On Your Favorite Team</h2>
         <hr></hr>
-        <div className="betting-container">
-
         <form>
+        <div className="betting-container">
           <div className="winner-selection">
-          <h3>1. Choose Winner</h3>
-            {/* TODO: Add real team data */}
-            <label>
-              CHI
-              <input type="radio" name="winning-team" value="home-team" />
-            </label>
-            <label>
-              NE
-              <input type="radio" name="winning-team" value="away-team" />
-            </label>
+            <h3>1. Choose Winner</h3>
+            <div>
+                <input type="radio" name="winning-team" value="home-team" id="home-team-selector"  className=" bet-selector hide" checked="checked" />
+                <label for="home-team-selector" className="bet-selector-btn">CHI
+                </label>
+            </div>
+            <div>
+              <input type="radio" name="winning-team" value="away-team" id="away-team-selector"
+                className="bet-selector hide" />
+              <label for="away-team-selector" 
+              className="bet-selector-btn">NE
+              </label>
+            </div>
           </div>
 
           <div className="slice-allocation">
             <h3>2. Place Your Slices</h3>
             <img src={PizzaWheel} />
-            <input type="range" min="1" max="8" />
+            <div className="bet-size-slider">
+              <input type="range" min="1" max="8" />
+              <span className="bet-size-indicator" >8</span>
+            </div>
           </div>
 
           <div className="comment-input">
-            <h3>3. Throw Some Cheese</h3>
-            <textArea type="text" name="comment"  />
+            <h3>3. Throw A Cheesy Comment</h3>
+            <textArea type="text" name="comment" className="input-comment-field" />
             <button className="button">Slice It</button>
           </div>
 
+            </div>
           </form>
-        </div>
         <h2>Discussion</h2>
         <hr></hr>
         <div className="comment">
           <img className="user-avatar" src={UserAvatar} />
-          <div className="comment-details">
-            <span className="username">ninjajon</span>
-            <img src={PizzaSlice} />
-            <span>5</span>
-            <img src={BearsLogo} />
-            <span>5 minutes ago</span>
-          </div>
           <div className="comment-body">
-            <p>Tom Brady is going to take this one home!!!</p> 
+            <div className="comment-details">
+              <span className="username">ninjajon</span>
+              <img src={PizzaSlice} />
+              <span>5</span>
+              <span> on</span>
+              <img className="bet-logo" src={BearsLogo} />
+              <span>5 minutes ago</span>
+            </div>
+            <div className="comment-text">
+              <p>Tom Brady is going to take this one home!!!</p> 
+            </div>
           </div>
         </div>
+
         <div className="comment">
           <img className="user-avatar" src={UserAvatar} />
-          <div className="comment-details">
-            <span className="username">ninjajon</span>
-            <img src={PizzaSlice} />
-            <span>5</span>
-            <img src={BearsLogo} />
-            <span>5 minutes ago</span>
-          </div>
           <div className="comment-body">
-            <p>Tom Brady is going to take this one home!!!</p> 
+            <div className="comment-details">
+              <span className="username">ninjajon</span>
+              <img src={PizzaSlice} />
+              <span>5</span>
+              <span> on</span>
+              <img className="bet-logo" src={BearsLogo} />
+              <span>5 minutes ago</span>
+            </div>
+            <div className="comment-text">
+              <p>Tom Brady is going to take this one home!!!</p> 
+            </div>
           </div>
         </div>
+
         <div className="comment">
           <img className="user-avatar" src={UserAvatar} />
-          <div className="comment-details">
-            <span className="username">ninjajon</span>
-            <img src={PizzaSlice} />
-            <span>5</span>
-            <img src={BearsLogo} />
-            <span>5 minutes ago</span>
-          </div>
           <div className="comment-body">
-            <p>Tom Brady is going to take this one home!!!</p> 
+            <div className="comment-details">
+              <span className="username">ninjajon</span>
+              <img src={PizzaSlice} />
+              <span>5</span>
+              <span> on</span>
+              <img className="bet-logo" src={BearsLogo} />
+              <span>5 minutes ago</span>
+            </div>
+            <div className="comment-text">
+              <p>Tom Brady is going to take this one home!!!</p> 
+            </div>
           </div>
         </div>
+
+        <div className="comment">
+          <img className="user-avatar" src={UserAvatar} />
+          <div className="comment-body">
+            <div className="comment-details">
+              <span className="username">ninjajon</span>
+              <span> 5</span>
+              <img src={PizzaSlice} />
+              <span> on</span>
+              <img className="bet-logo" src={BearsLogo} />
+              <span> 5 minutes ago</span>
+            </div>
+            <div className="comment-text">
+              <p>Tom Brady is going to take this one home!!!</p> 
+            </div>
+          </div>
+        </div>
+
+       
       </div>
       </div>
 
