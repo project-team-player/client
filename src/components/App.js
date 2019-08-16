d22f60ae6f512c48982155a38b4db080ffdb4b2d
Hans Martin <hansmhank@gmail.com>
2019-08-16 08:35:48 -0700

    Merged Brock's dev

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import GameThread from "../pages/GameThread";
+import Leaderboard from "../pages/Leaderboard";
+import Login from "../pages/Login";
+import Header from "./Header";
 import '../styles/main.css';
-import NavBar from './NavBar';
-import GameThread from './GameThread';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <GameThread />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={GameThread} />
+            <Route path="/leaderboard" component={Leaderboard} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
