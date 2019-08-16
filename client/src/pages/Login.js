0bc22ffcc241e72876c131f3b4434d894a13a6d3
Brock Jameson <bjameson@talentpath.com>
2019-08-15 19:53:00 -0700

    routes set, new pages made

---
+++ client/src/pages/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/Header";
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* <Header /> */}
+        <div className="Title">The routes are set</div>
+        <div className="imageContainer">
+          <img src="http://jonathancreamer.com/content/images/2017/01/asyncarnold.jpeg" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
