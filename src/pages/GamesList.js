896a15a1e33b5472cb667f9e5aa2e69e7f20f2f0
Hans Martin <hansmhank@gmail.com>
2019-08-16 09:51:18 -0700

    User flow working

--- src/pages/GamesList.js
+++ src/pages/GamesList.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import GameCard from '../components/GameCard';
+import GameThread from '../components/GameThread';
 
 // CSS
 import '../styles/GamesList.css';
+import { thisExpression, switchStatement } from '@babel/types';
 
 // Components
 
 class GamesList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showGameThread: false,
+    }
+
+    this.showGameThread = this.showGameThread.bind(this);
+    this.closeGameThread = this.closeGameThread.bind(this);
+  }
+
+  showGameThread() {
+    console.log('hey hey');
+    this.setState({ showGameThread: true});
+  }
+
+  closeGameThread() {
+    console.log('closing');
+    this.setState({ showGameThread: false});
+  }
+
   render() {
     return (
       <main>
+        <GameThread showModal={this.state.showGameThread} closeGameThread={this.closeGameThread} />
         {/* Ambigious Title Here */}
         <h2 className='GamesListTitle'>Game Threads Week 1</h2>
         <body className='GamesListGrid'>
-          <GameCard />
-          <GameCard />
-          <GameCard />
-          <GameCard />
-          <GameCard />
+          <GameCard showGameThread={this.showGameThread} />
+          <GameCard onClick={this.showGameThread} />
+          <GameCard onClick={this.showGameThread} />
+          <GameCard onClick={this.showGameThread} />
+          <GameCard onClick={this.showGameThread} />
         </body>
         {/* Components being maped out here 
-            Map out an array of objects
+          Map out an array of objects
             Destructure their attributes and pass them as state
             */}
       </main>
