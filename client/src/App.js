7b37079c5b4cadab3640253175f97cf692496228
Brock Jameson <bjameson@talentpath.com>
2019-08-15 19:53:00 -0700

    routes set, new pages made

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import GameThread from "./pages/GameThread";
+import Leaderboard from "./pages/Leaderboard";
+import Login from "./pages/Login";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={GameThread} />
+            <Route path="/leaderboard" component={Leaderboard} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
