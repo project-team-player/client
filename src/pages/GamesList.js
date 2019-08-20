35b1e96f36cdf40a42cb29a87ecc0e9f2a191e96
Jonathan Azali <azali.jonathan@gmail.com>
2019-08-20 10:17:16 -0700

    further updates

--- src/pages/GamesList.js
+++ src/pages/GamesList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GameCard from '../components/GameCard';
 import GameThread from '../components/GameThread';
+import axios from 'axios';
 
 // CSS
 import '../styles/GamesList.css';
@@ -13,7 +14,8 @@ class GamesList extends React.Component {
     super(props);
     this.state = {
       showGameThread: false,
-    }
+      games: []
+    };
 
     this.showGameThread = this.showGameThread.bind(this);
     this.closeGameThread = this.closeGameThread.bind(this);
@@ -21,34 +23,49 @@ class GamesList extends React.Component {
 
   showGameThread() {
     console.log('hey hey');
-    this.setState({ showGameThread: true});
+    this.setState({ showGameThread: true });
   }
 
   closeGameThread() {
     console.log('closing');
-    this.setState({ showGameThread: false});
+    this.setState({ showGameThread: false });
   }
+
+  getListOfGames = () => {
+    console.log('Getting List of Games');
+    axios
+      .get('https://pecorina-development.herokuapp.com/games/week/1')
+      .then(response => {
+        this.setState({ games: response.data.gamesList });
+      });
+    console.log(this.state.games);
+  };
 
   render() {
     return (
       <main>
-        <GameThread showModal={this.state.showGameThread} closeGameThread={this.closeGameThread} />
-        {/* Ambigious Title Here */}
+        <GameThread
+          showModal={this.state.showGameThread}
+          closeGameThread={this.closeGameThread}
+        />
         <h2 className='GamesListTitle'>Game Threads Week 1</h2>
+
+        <nav className='WeeksButtonContainer'>
+          <button className='WeeksButton' onClick={this.getListOfGames}>
+            Week 1
+          </button>
+        </nav>
+
         <body className='GamesListGrid'>
-          <GameCard showGameThread={this.showGameThread} />
-          <GameCard onClick={this.showGameThread} />
-          <GameCard onClick={this.showGameThread} />
-          <GameCard onClick={this.showGameThread} />
-          <GameCard onClick={this.showGameThread} />
-          <GameCard onClick={this.showGameThread} />
-          <GameCard onClick={this.showGameThread} />
-          <GameCard onClick={this.showGameThread} />
-        </body>
-        {/* Components being maped out here 
+          {/* Components being maped out here 
           Map out an array of objects
             take out their attributes and pass them as state
             */}
+          {this.state.games.map(game => {
+            `<h2>game.week</h2>`;
+          })}
+          <GameCard showGameThread={this.showGameThread} />
+        </body>
       </main>
     );
   }
