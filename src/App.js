f9752f959968d8378af3f1db7e7ac325b1fb6f5f
Jonathan Azali <azali.jonathan@gmail.com>
2019-08-20 10:17:16 -0700

    further updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+// Libraries imports here
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Games from './pages/Games';
+
+// Page Imports Here
 import Leaderboard from './pages/Leaderboard';
 import SignUp from './pages/SignupPage';
+
+// Component Imports Here
 import Header from './components/Header';
 import GamesList from './pages/GamesList';
+
+// CSS Imports Here
 import './styles/main.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      games: {}
+    };
+  }
+
   render() {
     return (
       <div className='container'>
         <BrowserRouter>
           <Header />
           <Switch>
-            <Route path='/' exact component={SignUp} />
+            <Route path='/' exact component={GamesList} />
             <Route path='/leaderboard' component={Leaderboard} />
             <Route path='/login' component={SignUp} />
-            <Route path='/gameslist' component={GamesList} />
           </Switch>
         </BrowserRouter>
       </div>
