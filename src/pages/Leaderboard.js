0931492e759edf92bfc71ce09a7e1b11d089aea5
Kevin Amarbayar <amarbayar.kevin@gmail.com>
2019-08-22 08:55:58 -0700

    yeet

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,13 +1,18 @@
-import React from "react";
-import Header from "../components/Header";
+import React from 'react';
+import Header from '../components/Header';
 
 class Leaderboard extends React.Component {
   render() {
     return (
       <div>
-        <div className="title">Wassup dudes</div>
-        <div className="imageContainer">
-          <img src="https://miro.medium.com/proxy/0*oCJbV_KPLl-1yGXU." />
+        <div className="title">Leaderboard</div>
+        <div className="container">
+          <div className="wrapper">
+            <section className="leaderboard">
+              <h3>supa</h3>
+            </section>
+            <aside className="friends">Friend 1</aside>
+          </div>
         </div>
       </div>
     );
