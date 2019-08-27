7a77907e098c6cc873333b8563d43c6870e08c5b
Jonathan Azali <azali.jonathan@gmail.com>
2019-08-27 13:18:11 -0700

    updates

--- src/components/OptionsButton.js
+++ src/components/OptionsButton.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
 // CSS
-import '../styles/GameCard.css';
 
 // Components
 
 class OptionsButton extends React.Component {
   render() {
     // consts here
-    //const {} = this.props;
-    return <option value='week 1'>week 1</option>;
+    const { weekNumber } = this.props;
+    return <option value='weekNumber'>week {weekNumber}</option>;
   }
 }
 
