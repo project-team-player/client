c92e52167d4792b41d6e4479c74e95ba8d0e1989
Jonathan Azali <azali.jonathan@gmail.com>
2019-08-27 09:37:40 -0700

    Options Component Created

---
+++ src/components/OptionsButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// CSS
+import '../styles/GameCard.css';
+
+// Components
+
+class OptionsButton extends React.Component {
+  render() {
+    // consts here
+    //const {} = this.props;
+    return <option value='week 1'>week 1</option>;
+  }
+}
+
+export default OptionsButton;
