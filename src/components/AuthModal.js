0902600b33e2414e3629ef7e88f0369c46e461b0
Hans Martin <hansmhank@gmail.com>
2019-09-03 15:14:57 -0700

    Working authentication, more secure

--- src/components/AuthModal.js
+++ src/components/AuthModal.js
@@ -139,16 +139,6 @@ class AuthModal extends Component {
     this.validateForm = this.validateForm.bind(this);
   }
 
-  // fireSuccessMessage(title, message) {
-  //   Swal.fire({
-  //     title: title,
-  //     text: message,
-  //     type: 'success',
-  //     showConfirmButton: false,
-  //   })
-  //   setTimeout(() => Swal.close(), 1500);
-  // }
-
   handleInputChange = event => {
     const { value, name } = (event.target);
     const formValues = {...this.state.formValues};
@@ -271,6 +261,7 @@ class AuthModal extends Component {
     const { loginView } = this.context.state;
     const { formValues, formErrors } = this.state;
 
+    // Conditionally renders login or signup form based on context state 
     return (
       <div className="auth-modal">
       {
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,8 +1,8 @@
 // Library imports here
 import React from 'react';
 import Cookies from 'universal-cookie';
+import axios from 'axios';
 const cookies = new Cookies();
-
 
 // Create global contexts
 const UserContext = React.createContext();
@@ -10,16 +10,28 @@ const UserContext = React.createContext();
 // Then create a provider Component
 class UserProvider extends React.Component {
   state = {
+    username: '',
+    email: '',
     isVisible: false,
     loginView: true,
     isLoggedIn: false,
   }
 
+  isUserLoggedIn = () => {
+    // Makes a call to backend to check if token is valid and sets isLoggedIn to true
+    const jwtCookie = cookies.get('bearerToken');
+    const token = jwtCookie.split('JWT ')[1];
+    axios({ method: 'POST', url: `${process.env.REACT_APP_PECORINA_SERVER_API}/authenticate`, headers: {authorization: `Bearer ${token}`}}).then(response => {
+      const { email, username } = this.state;
+      if (response.data.username) {
+        this.setState({ isLoggedIn: true, email, username });
+      }
+    });
+  }
+
   componentDidMount() {
-    if (cookies.get('bearerToken')) {
-      console.log('You are logged in!');
-      this.setState({isLoggedIn: true});
-    }
+    // Check if user is logged in
+    this.isUserLoggedIn();
   }
 
   render() {
@@ -36,10 +48,7 @@ class UserProvider extends React.Component {
           console.log('Logged out!');
         },
         isUserLoggedIn: () => {
-          if (cookies.get('bearerToken')) {
-            // this.setState({isLoggedIn: true});
-            return true;
-          }
+          this.isUserLoggedIn();
         },
         showModal: () => this.setState({isVisible: true}),
         hideModal: () => this.setState({isVisible: false}),
