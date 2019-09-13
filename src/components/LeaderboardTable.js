e923ade44136898dbb8b83a065b1f4253142ebac
Kevin Amarbayar <amarbayar.kevin@gmail.com>
2019-09-13 01:45:00 -0700

    final update

--- src/components/LeaderboardTable.js
+++ src/components/LeaderboardTable.js
@@ -1,86 +1,43 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-
+import React, { Component } from 'react';
+import PropTypes, { string } from 'prop-types';
 import '../styles/Leaderboard.css';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%',
-    marginTop: theme.spacing(3),
-    overflowX: 'auto',
-  },
-  table: {
-    minWidth: 1200, // 700 with friendboard
-  },
-}));
+class LeaderboardTable extends Component {
+  renderTableData() {
+    console.log(this.props.users);
+    const filteredUsers = this.props.users.filter(user => user.name.includes(this.props.query));
+    return filteredUsers.map((user, index) => {
+      const { name, wins, pizzaSlicesTotal } = user; // destructuring
+      return (
+        <tr>
+          <td className="index">{index}</td>
+          <td className="table-items">{name}</td>
+          <td className="table-items">{wins}</td>
+          <td>{pizzaSlicesTotal}</td>
+        </tr>
+      );
+    });
+  }
+  renderTableHeader() {
+    const header = ['Rank 🏅', 'Username 👻', '# of Wins per Week 🏆', 'Total Slices 🍕'];
+    return header.map((key, index) => <th key={index}>{key}</th>);
+  }
 
-function createData(rank, username, winperweek, totalslices) {
-  return { rank, username, winperweek, totalslices };
+  render() {
+    return (
+      <div className="leader-table">
+        <table id="users">
+          <tr className="table-header">{this.renderTableHeader()}</tr>
+          <tbody className="table-items">{this.renderTableData()}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
-const rows = [
-  createData('🥇', 'Superduperkevin', 10, 12304),
-  createData('🥈', 'Jon', 9, 11342),
-  createData('🥉', 'Balsagne', 8, 10002),
-  createData('4', 'Erv', 7, 10000),
-  createData('5', 'Hans', 6, 9999),
-  createData('6', 'Mahomes', 6, 8979),
-  createData('7', 'Rodgers', 6, 8888),
-  createData('8', 'Brady', 6, 8790),
-  createData('9', 'Wilson', 6, 6942),
-  createData('10', 'Maney', 6, 6941),
-];
+LeaderboardTable.propTypes = {
+  users: PropTypes.object,
+  query: PropTypes.string,
+};
 
-export default function SimpleTable() {
-  const classes = useStyles();
-
-  return (
-    <Paper className={classes.root}>
-      <Table
-        className={classes.table}
-        // data={
-        //   this.state.query
-        //     ? this.state.data.filter(x => !x.includes(this.state.query))
-        //     : this.state.data
-        // }
-      >
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">Rank</TableCell>
-            <TableCell align="center">Name</TableCell>
-            <TableCell align="center">
-              # of Wins per Week{' '}
-              <span role="img" aria-label="cup">
-                🏆
-              </span>
-            </TableCell>
-            <TableCell align="center">
-              Total Slices{' '}
-              <span role="img" aria-label="pizza">
-                🍕
-              </span>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <TableRow key={row.rank}>
-              <TableCell align="center" component="th" scope="row">
-                {row.rank}
-              </TableCell>
-              <TableCell align="center">{row.username}</TableCell>
-              <TableCell align="center">{row.winperweek}</TableCell>
-              <TableCell align="center">{row.totalslices}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
-}
+export default LeaderboardTable;
