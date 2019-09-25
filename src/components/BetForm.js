ab18d813c4dd109a39fb476ad2d7749857367d4f
Jonathan Azali <azali.jonathan@gmail.com>
2019-09-25 11:33:42 -0700

    bet selection finished

--- src/components/BetForm.js
+++ src/components/BetForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import Slider from './Slider';
 import TeamChoice from './TeamChoice';
 
@@ -6,49 +6,41 @@ class Comments extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-    };
+    this.state = {};
   }
 
-  makeGameBet(){
-      console.log('Game Bet Made');
+  makeGameBet() {
+    console.log('Game Bet Made');
   }
 
   render() {
     const { makeGameBet, gameDetails, handleBetChanges } = this.props;
     return (
-      <div className="betForm">
-          <h2>Make A Bet</h2>
-            <form onSubmit={makeGameBet}>
-                  <div className="betting-container">
+      <div className='betForm'>
+        <h2>Make A Bet</h2>
+        <form onSubmit={makeGameBet}>
+          <div className='bettingContainer'>
+            <Slider handleBetChanges={handleBetChanges} />
+            <div className='verticalLine'></div>
+            <TeamChoice
+              gameDetails={gameDetails}
+              handleBetChanges={handleBetChanges}
+            />
 
-                    <div className="slice-allocation">
+            <div className='comment-input'>
+              <h3>3. Throw A Cheesy Comment</h3>
+              <textarea
+                type='text'
+                name='comment'
+                className='input-comment-field'
+                onChange={handleBetChanges}
+              />
 
-                      <div className="bet-size-slider">
-                        {/* <img src={PizzaWheel} /> */}
-                        {/* <input type="range" min="1" max="8" /> */}
-                        {/* <span className="bet-size-indicator">8</span> */}
-                        <Slider handleBetChanges={handleBetChanges}/>
-                      </div>
-                    </div>
-
-                    <TeamChoice gameDetails={gameDetails} handleBetChanges={handleBetChanges} />
-
-                    <div className="comment-input">
-                      <h3>3. Throw A Cheesy Comment</h3>
-                      <textarea
-                        type="text"
-                        name="comment"
-                        className="input-comment-field"
-                        onChange={handleBetChanges}
-                      />
-                      
-                      <button className="button">Slice It</button>
-                      
-                    </div>
-                </div>
-            </form>
-            <hr />
+              <button className='button'>Slice It</button>
+            </div>
+          </div>
+        </form>
+        <hr />
       </div>
     );
   }
