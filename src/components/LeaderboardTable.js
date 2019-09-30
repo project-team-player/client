046de377b8ffa02d8b567d38202142f0e6ec246f
Kevin Amarbayar <amarbayar.kevin@gmail.com>
2019-09-30 09:45:10 -0700

    yeet

--- src/components/LeaderboardTable.js
+++ src/components/LeaderboardTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes, { string } from 'prop-types';
+import PropTypes from 'prop-types';
 import '../styles/Leaderboard.css';
 
 class LeaderboardTable extends Component {
