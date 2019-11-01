0b0f8184f97b928c563d3d9d128b478d646c2bd3
Brock Jameson <bjameson@talentpath.com>
2019-11-01 10:46:04 -0700

    google analytics with page tracking setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // Libraries imports here
 import React, { useState, useContext } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
 
 // import react contexts
 import { UserProvider, AuthContext, UserContext } from "./contexts/UserContext";
@@ -17,6 +19,25 @@ import GamesList from "./pages/GamesList";
 // CSS Imports Here
 import "./styles/main.css";
 import AuthModal from "./components/AuthModal";
+
+// ReactGA.set({
+// userId: this.props.context.state.user.name,
+// userEmail: this.props.context.state.user.email
+// any data that is relevant to the user session
+// that you would like to track with google analytics
+// });
+
+// Google Analytics Setup
+ReactGA.initialize("UA-151338746-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+// Initialize google analytics page view tracking
+const history = createBrowserHistory();
+
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 const App = () => {
   return (
@@ -42,9 +63,23 @@ const App = () => {
                     }
                   >
                     <Switch>
-                      <Route path="/" exact component={Splash} />
-                      <Route path="/games" exact component={GamesList} />
-                      <Route path="/leaderboard" component={Leaderboard} />
+                      <Route
+                        path="/"
+                        exact
+                        component={Splash}
+                        history={history}
+                      />
+                      <Route
+                        path="/games"
+                        exact
+                        component={GamesList}
+                        history={history}
+                      />
+                      <Route
+                        path="/leaderboard"
+                        component={Leaderboard}
+                        history={history}
+                      />
                       <Route
                         path="/login"
                         render={props => <AuthModal modal={false} />}
