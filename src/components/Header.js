c567baab5edb0bb3a1006cce2302f1861f7bb877
Hans Martin <hansmhank@gmail.com>
2019-11-01 12:28:12 -0700

    Changed user icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,23 @@ import "../styles/Header.css";
 import logo from "../images/slice-it-logo.svg";
 import slice from "../images/logo-slice.svg";
 // import logotext from "../images/logo-text.svg";
-import loginprofile from "../images/login.jpg";
+import User from "../images/user.svg";
 import { NavLink } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 import PizzaSlice from "../images/logo.svg";
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDropdown: false,
+    }
+  }
+
+  toggleDropdown = () => {
+    this.setState({ showDropdown: !this.state.showDropdown });
+  }
+
   render() {
     return (
       <UserContext.Consumer>
@@ -33,29 +44,39 @@ class Header extends React.Component {
                 </li>
                 {/* will be NavLinks when routes are set */}
                 {context.state.isLoggedIn ? (
-                  <>
-                  {/* <div className="logoutPizzaCount"> */}
-                    <button className="login-button" onClick={context.logOut}>
-                      {/* <img className="profileimg" src={loginprofile}></img> */}
-                      <span className="logouttext">Log Out</span>
-                      {/* <div className="pizzaContainer">
-                      <img
-                        className="pizzalogo"
-                        src={PizzaSlice}
-                        alt="slice-it-logo"
-                      />
-                      <span className="slices">
-                        {context.state.user.pizzaSlicesWeekly}
-                      </span>
-                        </div> */}
-                    </button>
-                    
-                  {/* </div> */}
-                  </>
+                    <div className="accountDropdown">
+                      <div className="accountButtonContainer">
+                      <button className="accountButton" onClick={this.toggleDropdown}>
+                        <img className="accountIcon" src={User}></img>
+                        {/* <span className="logouttext">Log Out</span> */}
+                        <div className="pizzaContainer">
+                          <img
+                            className="pizzalogo"
+                            src={PizzaSlice}
+                            alt="slice-it-logo"
+                          />
+                          <span className="slices">
+                            {context.state.user.pizzaSlicesWeekly}
+                          </span>
+                        </div>
+                      </button>
+                      </div>
+                      {
+                        this.state.showDropdown ?
+                        <div className="accountDropdownContent">
+                          <div className="accountDropdownContentInner">
+                          <button className="logOutButton" onClick={context.logOut}>Log Out</button>
+                          </div>
+                        </div> 
+                        :
+                        ""
+                      }
+
+                    </div>
                 ) : (
-                  <button className="login-button" onClick={context.showModal}>
+                  <button className="loginButton" onClick={context.showModal}>
                     <span className="logintext">Log In</span>
-                    <img className="profile" src={loginprofile} alt="Login Icon"></img>
+                    <img className="profile" src={User} alt="Login Icon"></img>
                   </button>
                 )}
               </ul>
