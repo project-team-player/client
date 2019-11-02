6bca52f4a508278187319f1f9c66b82a47f955ce
Hans Martin <hansmhank@gmail.com>
2019-11-02 12:27:21 -0700

    Fixed bug where gamethread is hidden and added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,20 @@
 import React from "react";
+import '../styles/Footer.css'
+import Logo from "../images/slice-it-logo.svg";
 
-class Footer extends React.Component {
-  render() {
+const Footer = () => {
     return (
-      <div>
-        <div>
-          <div>This is our footer lol</div>
+      <footer className="footer">
+        <div className="footerInner">
+         <img src={Logo} alt="Slice it logo" />
+         <p>® 2019 Slice It - Product of <a href="https://www.talentpath.com" target="_blank" className="tpLink">Talent Path</a></p>
+          
+
+          <a className="feedbackLink" href="https://docs.google.com/forms/d/1zEwEIBv_OVq0OJ9pEKJgHPg160Mz3yHK21VrutrX9NY/viewform?edit_requested=true" target="_blank" >Submit Feedback</a>
         </div>
-      </div>
+        
+      </footer>
     );
-  }
 }
 
 export default Footer;
